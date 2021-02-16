d815ce84495c0330f78cad8fcc503670356539ff
LunaMindy <santacandy@naver.com>
2021-02-16 14:54:50 +0900

    Java34

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -6,6 +6,8 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello java!");
 		System.out.println("Hello java2");
+		System.out.println("Hello java3");
+		System.out.println("Hello java4");
 	}
 
 }
