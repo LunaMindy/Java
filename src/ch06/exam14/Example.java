40a612184d24fa155f32b220c0267b79e9e564bf
LunaMindy <santacandy@naver.com>
2021-02-22 14:58:18 +0900

    121

---
+++ src/ch06/exam14/Example.java
@@ -0,0 +1,16 @@
+package ch06.exam14;
+
+public class Example {
+
+	public static void main(String[] args) {
+
+		
+		//Korean.NATION = "미국"; //final때문에 불가능
+
+		Korean k1 = new Korean("123456-1234567");
+		//k1.ssn = "666422-1234556";//final 때문에 안된다.(x)
+	
+	
+	}
+
+}
---
+++ src/ch06/exam14/Korean.java
@@ -0,0 +1,14 @@
+package ch06.exam14;
+
+public class Korean {
+		//Field
+		final static String NATION = "한국"; //다 갖고 있어서 static
+		final String ssn; ////ssn 값을 입력해줘야하는데 다같은 주민번호를 줄수없다. 생성자로 이용한다.
+		
+		//Constructor
+		Korean(String ssn){
+			
+			this.ssn = ssn;
+		}
+		//Method
+}
